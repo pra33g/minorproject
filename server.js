2f6e6ef858b457d03eb41d0bc09453e7e003d710
pra33g <prakhargupta3301@gmail.com>
2022-12-31 00:10:49 +0530

    port changed on branch

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require('express');
 const upload = require('express-fileupload');
 const app = express();
 const router = require('./routes/bookmark');
-const port = 3000;
+const port = 5000;
 app.listen(port, ()=>console.log("Port " + port));
 
 //log requests
