c39d509a2356dea4501800e212ca8866d7b53833
pra33g <prakhargupta3301@gmail.com>
2023-01-01 18:43:22 +0530

    progress added

--- public/bookmark/script.js
+++ public/bookmark/script.js
@@ -1,22 +1,40 @@
 /*jshint esversion: 8 */
+//send file to server
 $('#form_bookmark').submit(
     function( e ) {
-        // console.log(e);
-        $.ajax( {
-            url: '/bookmark',
-            type: 'POST',
-            data: new FormData( this ),
-            processData: false,
-            contentType: false,
-            dataType: "json",
-            success: function(result, status, jqXHR){
-                console.log(result, status);
-                //$("#div1").html(str);
-            }
-        } );
         e.preventDefault();
+        let formData = new FormData(this);
+        console.log(formData);
+        let xhr = new XMLHttpRequest();
+        xhr.upload.addEventListener("progress", progressHandler);
+        xhr.open("POST", "/bookmark");
+        xhr.send(formData);
+        xhr.upload.addEventListener("load", successUpload);
+
+        //get response from server in json and log it
+        xhr.onreadystatechange = ()=>{
+            if(xhr.readyState == XMLHttpRequest.DONE){
+                xhr.responseType = 'json';
+                console.log(xhr.response);
+            }
+        };
+
     } 
 );
+//listen for progress updates
+function progressHandler(event){
+    let totalSize = event.total;
+    let loadedSize = event.loaded;
+
+    let pStatus = document.getElementById("uploadedBytes");
+    pStatus.innerHTML = `Uploaded: ${loadedSize/totalSize * 100}`; 
+}
+//successful completion
+function successUpload(event){
+    let pStatus = document.getElementById("uploadedBytes");
+    pStatus += "DONE";
+}
+
 const source = new EventSource('/bookmark/bookmarkStatus');
 source.addEventListener("message", message => {
     console.log("Got ", JSON.parse(message.data));
